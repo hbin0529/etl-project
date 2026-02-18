bc49997d7802fc950589f5d2f208c6eb67172e37
hbin0529 <small052@naver.com>
2026-02-19 00:09:56 +0900

    feat: generate_data 기본 구조 생성

---
+++ src/generate_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+# generate_orders 함수 정의
+def generate_orders():
+    pass
+    # order_id
+    # customer_id
+    # order_date
+    # product
+    # category
+    # price - 일부 NaN 넣기
+    # quantity - 일부 0 또는 음수 넣기
+    # total_amount
+
+
+# save 함수 정의
+def save():
+    pass
+
+# main 함수
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+####### 랜덤 데이터 전략 세우기 #######
+# 날짜는 어떻게 랜덤 생성할 건가?
+
+# 상품은 리스트로 둘 건가?
+
+# 카테고리는 매핑할 건가?
+
+# 가격은 정수? float?
+
+# 수량 범위는?
+
+# 일부 결측치는 어떻게 넣을 건가?
