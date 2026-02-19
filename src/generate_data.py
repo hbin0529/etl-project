b360318f6b5cfd632690f1c7d725864326a2b8cf
hbin0529 <small0529@naver.com>
2026-02-19 16:29:29 +0900

    Enter unrefined data

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,17 +1,69 @@
 import pandas as pd
+import numpy as np
 
 # generate_orders 함수 정의
+# 직접 데이터 입력
 def generate_orders():
     pass
-    # order_id
-    # customer_id
-    # order_date
-    # product
-    # category
-    # price - 일부 NaN 넣기
-    # quantity - 일부 0 또는 음수 넣기
-    # total_amount
 
+data = {
+    # 주문번호
+    "order_id": [1, 2, 3, 4, 5],
+
+    # 주문자
+    "customer_id": ["C001", "C002", "", "C004", None],
+
+    # 주문일자:
+    "order_date": [
+        "2025-01-10",
+        "2025-01-11",
+        "",                 # 결측
+        "2025-01-13",
+        "2025-01-14"
+    ],
+    # 상품명
+    "product": [
+        "Laptop",
+        "Mouse",
+        "Keyboard",
+        "Monitor",
+        ""
+    ],
+    # 카테고리
+    "category": [
+        "Electronics",
+        "Electronics",
+        "Electronics",
+        "",
+        "Accessories"
+    ],
+    # 가격: 정상 + NaN
+    "price": [
+        1200.0,
+        25.5,
+        np.nan,   # NaN
+        300.0,
+        -50.0     # 이상값(음수)
+    ],
+    # 수량
+    "quantity": [
+        1,
+        2,
+        0,    # 이상값
+        -1,   # 이상값
+        3
+    ],
+    # 총 금액
+    "total_amount": [
+        1200.0,    # 정상
+        51.0,      # 정상
+        0.0,       # 계산 불가 (price NaN)
+        -300.0,    # 잘못된 값
+        None       # 결측
+    ],
+}
+
+df = pd.DataFrame(data)
 
 # save 함수 정의
 def save():
@@ -19,7 +71,7 @@ def save():
 
 # main 함수
 def main():
-    pass
+    print(df)
 
 if __name__ == "__main__":
     main()
